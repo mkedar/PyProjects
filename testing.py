06debdd2fc451eaa9ee6c5a63e0e179f7fe29ec3
mkedar <78320557+mkedar@users.noreply.github.com>
2023-10-03 16:14:20 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -11,20 +11,21 @@ xdata, ydata = [], []
 ln, = plt.plot([], [], 'r-', animated=True)
 
 def init():
-    ax.set_xlim(0, 60) 
+    ax.set_xlim(0, 10) 
     ax.set_ylim(30, 80)  
     return ln,
 
 def update(frame):
     xdata.append(frame)
     ydata.append(fetch_temperature())
-    if len(xdata) > 60: 
-        del xdata[0]
-        del ydata[0]
-    ax.set_xlim(min(xdata), max(xdata))
+
+    if len(xdata) <= 10:
+        ax.set_xlim(1, 10)
+    else:
+        ax.set_xlim(min(xdata), max(xdata))
+
     ln.set_data(xdata, ydata)
     return ln,
 
 ani = FuncAnimation(fig, update, frames=range(1000), init_func=init, blit=True)
 plt.show()
-
